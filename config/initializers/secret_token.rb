2a79a59025dc0c6b3cd0ab2c844dea49f7b63a38
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-09-26 11:01:30 +0700

    create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiftLaw::Application.config.secret_token = '617c77a1914f19047d75e226bb1bac4ebefefa80457b73d999bb54ea76df8447feb1039da07384cb0fb5056e41a423a71260ddbf0b546531a8dad40320ed76e1'
