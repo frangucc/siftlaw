28a954ad93bd6daa1ff221581fa225eaa2755f53
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-10-06 21:15:23 +0700

    email form - search results page

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
 
   def feedback(from_email, to_email, subject, content)
-    @from_email
+    @from_email = from_email
     @content = content
     mail(from: from_email, to: to_email, subject: subject)
   end
